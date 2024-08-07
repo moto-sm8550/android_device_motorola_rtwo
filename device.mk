be8ddd9309d45eae8ed9bc6620415135e130b23d
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-08 00:39:42 +0300

    rtwo: remove device-specific sensors
    Change-Id: Icf2abe5dabf60742824922ab027d912a2b6a8753

--- device.mk
+++ device.mk
@@ -76,13 +76,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
-# Sensors
-PRODUCT_PACKAGES += \
-    sensors.rtwo
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-rtwo-game-perf.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-rtwo.conf
