3dd2d652e287c4fc74c164a7ecbb87b921fb1662
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-07 18:57:45 +0300

    rtwo: remove device-specific powershare
    Change-Id: Id499251c94ffbaa79c0a168dbf6dcb877e049dad

--- device.mk
+++ device.mk
@@ -77,10 +77,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
-# PowerShare
-PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.rtwo
-
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.rtwo
