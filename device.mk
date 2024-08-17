d32ff2f3da3ab3426ac75b6df817738b7961cf15
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 04:59:30 +0300

    rtwo: do not manually build dependencies
    Change-Id: I4046d8eeabfc70842898b7034f75cdd6bcfc441b

--- device.mk
+++ device.mk
@@ -66,10 +66,7 @@ PRODUCT_COPY_FILES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2.vendor \
-    android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
-    libchrome.vendor \
     nqnfcinfo \
     Tag
 
