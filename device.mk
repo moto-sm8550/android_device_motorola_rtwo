48750d6f23560aac1693daec8ae6497dca3a8956
Bruno Martins <bgcngm@gmail.com>
2025-01-27 09:45:36 +0000

    rtwo: Rename rootdir to init and migrate to blueprints
    Change-Id: I9d55dc634fc4df2c60d30a0c18ec356a857e3c4b

--- device.mk
+++ device.mk
@@ -57,12 +57,8 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.rtwo
 
 # Init
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/hw/*.rc),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/$(notdir $f)))
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/*.rc),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/$(notdir $f)))
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/bin/$(notdir $f)))
+PRODUCT_PACKAGES += \
+    init.mmi.overlay.rc
 
 # Keylayout
 PRODUCT_COPY_FILES += \
