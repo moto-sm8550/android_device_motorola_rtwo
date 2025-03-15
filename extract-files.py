0f70c32b9c29a79473ac42cf0228bd653fc7ac52
kyasu <kyasu.dev@gmail.com>
2025-03-15 18:56:22 +0900

    rtwo: Shim nfc_nci.nqx.default.hw.so with libbase_shim
    nfc_nci.nqx.default.hw.so: error: Unresolved symbol: _ZN7android4base4TrimERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE
    
    Change-Id: Iaa961f274c0e8d5dad47adc4e0a81c35ed8ab550

--- extract-files.py
+++ extract-files.py
@@ -37,6 +37,8 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'product/priv-app/MotCamera4/MotCamera4.apk': blob_fixup()
         .apktool_patch('MotCamera4-patches'),
+    'vendor/lib64/nfc_nci.nqx.default.hw.so': blob_fixup()
+    .add_needed('libbase_shim.so'),
 }  # fmt: skip
 
 extract_fns: extract_fns_user_type = {
