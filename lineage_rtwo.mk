2c07d3288c0d5ac2ecb284c74551bd75817a0ecf
Marc Bourgoin <themard85@gmail.com>
2025-06-16 10:34:39 -0600

    rtwo: Update vendor from rtwo_g-user 15 V1TRS35H.60-33-5 7195d7-c0632
    Change-Id: I18585198e034f1187e8a969211649546091abc29

--- lineage_rtwo.mk
+++ lineage_rtwo.mk
@@ -22,6 +22,6 @@ PRODUCT_MODEL := motorola edge 40 pro
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="rtwo_g-user 15 V1TR35H.60-33 d7ecee-e847d6 release-keys" \
-    BuildFingerprint=motorola/rtwo_g/rtwo:15/V1TR35H.60-33/d7ecee-e847d6:user/release-keys \
+    BuildDesc="rtwo_g-user 15 V1TRS35H.60-33-5 7195d7-c0632 release-keys" \
+    BuildFingerprint=motorola/rtwo_g/rtwo:15/V1TRS35H.60-33-5/7195d7-c0632:user/release-keys \
     DeviceProduct=rtwo_g
