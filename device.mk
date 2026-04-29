0f86c7a54ec057bf89fc9d698149eaf9972d43ef
Nick Reuter <nreuter85@gmail.com>
2026-04-28 23:01:00 -0500

    rtwo: Declare support for AntiFlicker
    Change-Id: If4f7776a104c398e3f7d5c86ac06042880f38aec

--- device.mk
+++ device.mk
@@ -63,6 +63,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/goodix_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/goodix_ts.kl
 
+# LiveDisplay
+$(call soong_config_set_bool,livedisplay_sysfs,enable_af,true)
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp \
